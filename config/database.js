0e812633639c0205f9c39723fb7b2e40ebcf135b
myyusuf <myyusuf1911@gmail.com>
2016-10-31 08:14:25 +0700

    First commit

--- config/database.js
+++ config/database.js
@@ -2,7 +2,6 @@
 module.exports = {
         'mysqlConnectionData' : {
         host: 'localhost',
-        // host: '128.199.89.79',
         user: 'root',
         password: 'hellopower11',
         database: 'ceu_db'
