fdcae3435d3359f1db99a8cb713067c8277ba73f
myyusuf <myyusuf1911@gmail.com>
2016-10-31 16:24:54 +0700

    Add order by to student list all

--- app/handlers/student_data.js
+++ app/handlers/student_data.js
@@ -38,7 +38,7 @@ exports.listAll = function(req, res, db) {
   if(req.param('level')){
     var studentLevel = parseInt(req.param('level'));
 
-    var query = "SELECT * FROM tb_siswa WHERE tingkat = ? ";
+    var query = "SELECT * FROM tb_siswa WHERE tingkat = ? order by nama ";
 
     db.query(
       query, [studentLevel],
@@ -48,7 +48,7 @@ exports.listAll = function(req, res, db) {
       }
     );
   }else{
-    var query = "SELECT * FROM tb_siswa ";
+    var query = "SELECT * FROM tb_siswa order by nama ";
 
     db.query(
       query, [],
