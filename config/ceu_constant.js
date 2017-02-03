1d3b577e3e5973943e4b4b3d9c8c8d10106d3b18
myyusuf <myyusuf1911@gmail.com>
2017-02-03 22:50:23 +0700

    add ceu_constant for mppd

--- config/ceu_constant.js
+++ config/ceu_constant.js
@@ -1,6 +1,10 @@
 module.exports = {
 
-	'MEDICAL_INFO_DIR' : '/Users/myyusuf/Documents/Test/ceu_files/medical_info/',
-	'PERMISSION_INFO_DIR' : '/Users/myyusuf/Documents/Test/ceu_files/permission_info/',
-	'LEAVE_INFO_DIR' : '/Users/myyusuf/Documents/Test/ceu_files/leave_info/'
+	// 'MEDICAL_INFO_DIR' : '/Users/myyusuf/Documents/Test/ceu_files/medical_info/',
+	// 'PERMISSION_INFO_DIR' : '/Users/myyusuf/Documents/Test/ceu_files/permission_info/',
+	// 'LEAVE_INFO_DIR' : '/Users/myyusuf/Documents/Test/ceu_files/leave_info/'
+
+	'MEDICAL_INFO_DIR' : '/var/ceu_files/medical_info/',
+	'PERMISSION_INFO_DIR' : '/var/ceu_files/permission_info/',
+	'LEAVE_INFO_DIR' : '/var/ceu_files/leave_info/'
 };
